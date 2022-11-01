ce613190e21d9fe10c41f0ef4e381dfb60327719
developingsofts <63606042+developingsofts@users.noreply.github.com>
2022-11-01 19:47:01 +0530

    todo app
    todo app

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Todo List
+airtable  login info 
+email:werapidsofts@gmail.com
+pass:123123
+website link :https://airtable.com/appdVn7JLNCM4X7TT/tblVgh5PqpMJDUPul/viwXuUitlstUyDZ2d?blocks=hide
---
+++ src/App.js
@@ -0,0 +1,213 @@
+import {
+  Heading,
+  IconButton,
+  VStack,
+  useColorMode,
+  useToast,
+} from "@chakra-ui/react";
+import TaskList from "./components/tasks";
+import AddTask from "./components/AddTask";
+import { FaSun, FaMoon } from "react-icons/fa";
+import { useState, useEffect } from "react";
+
+function App() {
+  const toast = useToast();
+  // const [tasks, setTasks] = useState(
+  //   () => JSON.parse(localStorage.getItem("tasks")) || []
+  // );
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [tasks, setTasks] = useState([]);
+
+  const getToDoList=async()=>{
+    fetch('https://api.airtable.com/v0/appdVn7JLNCM4X7TT/ToDoList?api_key=keyAC0Xixg5V0AgFU')
+    .then((resp) => resp.json())
+    .then(data => {
+    console.log('GetData.response==', JSON.stringify(data?.records))
+    setTasks(data?.records)
+    }).catch(err => {
+      
+    });
+  
+  }
+  useEffect(()=>{
+    getToDoList()
+  },[])
+  const createTask=async(task)=>{
+    console.log('payload==', task?.body)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' ,'Authorization':'Bearer keyAC0Xixg5V0AgFU'},
+      body: JSON.stringify({
+        "fields": {'TaskName': task?.body}
+      })
+  };
+    fetch('https://api.airtable.com/v0/appdVn7JLNCM4X7TT/ToDoList',requestOptions)
+    .then((resp) => resp.json())
+    .then(data => {
+    console.log('createTask.response==', JSON.stringify(data))
+    addTask(data)
+  
+    }).catch(err => {
+      console.log('createTask.errro==', err)
+    });
+  }
+  
+  const updateTask=async(task, onClose)=>{
+     //tasks[selectedIndex] = task
+
+   onClose();
+    const requestOptions = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' ,'Authorization':'Bearer keyAC0Xixg5V0AgFU'},
+     body:JSON.stringify({
+      "fields": {
+        "TaskName": task?.fields?.TaskName
+      }
+    })
+  };
+   fetch('https://api.airtable.com/v0/appdVn7JLNCM4X7TT/ToDoList/'+task?.id,requestOptions)
+    .then((resp) => resp.json())
+    .then(data => {
+    console.log('updateTask.response==', JSON.stringify(data))
+    getToDoList()
+    // setTimeout(()=>{
+    //   setTasks([...tasks])
+    // },100)
+    //addTask(data)
+  
+    }).catch(err => {
+      console.log('updateTask.errror==', err)
+    });
+  }
+
+  const selectedTaskIndex=(index)=>{
+    setSelectedIndex(index)
+    console.log('selectedTaskIndex===',index)
+  }
+
+
+
+  // useEffect(() => {
+   
+  //   localStorage.setItem("tasks", JSON.stringify(tasks));
+  // }, [tasks]);
+
+  // function deleteTask(id) {
+  //   const newTasks = tasks.filter((task) => {
+  //     return task.id !== id;
+  //   });
+  //   setTasks(newTasks);
+  // }
+
+  function deleteTask(id,onClose) {
+    onClose()
+    console.log('deleteTask===',id)
+    const requestOptions = {
+      method: 'DELETE',
+      headers: { 'Authorization':'Bearer keyAC0Xixg5V0AgFU'},
+    
+  };
+  console.log('updateTask.payload==',requestOptions )
+    fetch('https://api.airtable.com/v0/appdVn7JLNCM4X7TT/ToDoList/'+id,requestOptions)
+    .then((resp) => resp.json())
+    .then(data => {
+    console.log('deleteTask.response==', JSON.stringify(data))
+    const newTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(newTasks);
+  
+    }).catch(err => {
+      console.log('deleteTask.errro==', err)
+    });
+  
+   
+  }
+
+
+  function deleteTaskAll() {
+    setTasks([]);
+  }
+
+  function checkTask(id) {
+    const newTasksCheck = tasks.map((task, index, array) => {
+      if (task.id === id) {
+        task.check = !task.check;
+      }
+      return task;
+    });
+    setTasks(newTasksCheck);
+  }
+
+  // function updateTask(id, body, onClose) {
+  //   const info = body.trim();
+
+  //   if (!info) {
+  //     toast({
+  //       title: "Enter your task",
+  //       position: "top",
+  //       status: "warning",
+  //       duration: 2000,
+  //       isClosable: true,
+  //     });
+
+  //     return;
+  //   }
+
+  //   const newTasksUpdate = tasks.map((task, index, array) => {
+  //     if (task.id === id) {
+  //       task.body = body;
+  //       task.check = false;
+  //     }
+  //     return task;
+  //   });
+
+  //   setTasks(newTasksUpdate);
+
+  //   onClose();
+  // }
+
+  function addTask(task) {
+    setTasks([task,...tasks]);
+    console.log('createTask.tasks==', JSON.stringify(tasks))
+  }
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <VStack p={4} minH='100vh' pb={28}>
+      {/* <IconButton
+        icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
+        isRound='true'
+        size='md'
+        alignSelf='flex-end'
+        onClick={toggleColorMode}
+         aria-label='toogle-dark-mode'
+      /> */}
+
+      {/* <Heading
+        p='5'
+        fontWeight='extrabold'
+        size='xl'
+        bgGradient='linear(to-r, red.100, yellow.100)'
+        bgClip='text'
+      >
+        Todo list
+      </Heading> */}
+      <AddTask 
+      //addTask={addTask}
+      addTask={createTask}
+      />
+      <TaskList
+      selectedTaskIndex={selectedTaskIndex}
+        tasks={tasks}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+        deleteTaskAll={deleteTaskAll}
+       // checkTask={checkTask}
+      />
+    </VStack>
+  );
+}
+
+export default App;
